d07fe60941574db6e8b73bb81717f8456c48edbb
elirankochen <elirankochen@gmail.com>
2021-02-27 13:47:39 +0200

    Add files via upload

--- status/status.py
+++ status/status.py
@@ -14,29 +14,40 @@ class StatusEnum:
     COMPLETE = {'order': 3, 'slug': 'Complete', 'description': 'the scan was completed successfully'}
     NOT_FOUND = {'order': 4, 'slug': 'Not found', 'description': 'the scan-id could not be found'}
 
+    @staticmethod
+    def get_statuses():
+        return [StatusEnum.ACCEPTED, StatusEnum.RUNNING, StatusEnum.ERROR, StatusEnum.COMPLETE,
+                StatusEnum.NOT_FOUND]
+
 
 @app.route('/scan_status/<scan_id>', methods=['GET'])
 def get_scan_status(scan_id):
-    status = None
-    pending_scans_items = redis.lrange('scan_items', 0, -1)
-    pending_scans_ids = [json.loads(scan_item)['scan_id'] for scan_item in pending_scans_items]
-
-    if scan_id in pending_scans_ids:
-        status = StatusEnum.ACCEPTED
-
-    elif redis.sismember('running_scans', scan_id):
-        status = StatusEnum.RUNNING
-
-    elif redis.sismember('success_tasks', scan_id):
-        status = StatusEnum.COMPLETE
-
-    elif redis.sismember('failed_tasks', scan_id):
-        status = StatusEnum.ERROR
-
-    if not status:
-        status = StatusEnum.NOT_FOUND
-
+    status = StatusLogic.get_scan_status(scan_id)
     return status, 200
+
+
+class StatusLogic:
+    @staticmethod
+    def get_scan_status(scan_id):
+        status = None
+        pending_scans_items = redis.lrange('scan_items', 0, -1)
+        pending_scans_ids = [json.loads(scan_item)['scan_id'] for scan_item in pending_scans_items]
+        if scan_id in pending_scans_ids:
+            status = StatusEnum.ACCEPTED
+
+        elif redis.sismember('running_scans', scan_id):
+            status = StatusEnum.RUNNING
+
+        elif redis.sismember('success_tasks', scan_id):
+            status = StatusEnum.COMPLETE
+
+        elif redis.sismember('failed_tasks', scan_id):
+            status = StatusEnum.ERROR
+
+        if not status:
+            status = StatusEnum.NOT_FOUND
+
+        return status
 
 
 if __name__ == '__main__':
