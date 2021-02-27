105bf229c72424c80d6d66109d6ab1b0dcd48502
elirankochen <elirankochen@gmail.com>
2021-02-27 14:53:03 +0200

    Add files via upload

--- process/process_logic.py
+++ process/process_logic.py
@@ -8,6 +8,7 @@ class ProcessLogic:
     def save_scanner_status(scan_item, status_succeeded):
         redis.sadd('success_tasks', scan_item['scan_id']) if status_succeeded \
             else redis.sadd('failed_tasks', scan_item['scan_id'])
+        
     @staticmethod
     def run_scanner(scan_item):
         # run scanner will identify if it is a consecutive scans according to scan_services_id
